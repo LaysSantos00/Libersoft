0e50cd0154bdbc2254bdd949bd58fba25faf5661
LaysSantos00 <laysdias00@gmail.com>
2021-11-10 19:50:06 -0300

    implemtantação da tela de login

--- src/main/java/com/libersoft/DAO/BibliotecarioDAO.java
+++ src/main/java/com/libersoft/DAO/BibliotecarioDAO.java
@@ -1,6 +1,7 @@
 package com.libersoft.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.libersoft.Model.Bibliotecario;
@@ -9,4 +10,7 @@ import com.libersoft.Model.Bibliotecario;
 public interface BibliotecarioDAO extends JpaRepository<Bibliotecario, Integer> {
 	boolean existsByEmail(String email);
 	boolean existsByCpf(String cpf);
+	
+	@Query("SELECT u FROM Bibliotecario u WHERE u.cpf = :cpf AND u.senha = :senha")
+	public Bibliotecario findByLoginAndSenha(String cpf, String senha);
 }
---
+++ src/main/java/com/libersoft/Login/LoginController.java
@@ -0,0 +1,50 @@
+package com.libersoft.Login;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.libersoft.DAO.BibliotecarioDAO;
+import com.libersoft.DAO.LivroDAO;
+import com.libersoft.Model.Bibliotecario;
+
+
+@Controller
+public class LoginController {
+	@Autowired
+	private BibliotecarioDAO bibliotecarioDAO;
+	@Autowired
+	private LivroDAO livroDAO;
+
+	@GetMapping("/")
+	public String exibirLogin() {
+		return "login";
+	}
+
+	@PostMapping("/fazerLogin")
+	public String fazerLogin(String cpf, String senha, RedirectAttributes ra, HttpSession session) {
+		Bibliotecario bibliotecarioLogado = this.bibliotecarioDAO.findByLoginAndSenha(cpf, senha);
+		if (bibliotecarioLogado == null) {
+			ra.addFlashAttribute("mensagem", "Cpf ou senha inválidos");
+			return "redirect:/";
+		} else {
+			session.setAttribute("bibliotecarioLogado", bibliotecarioLogado);
+			return "redirect:/bibliotecario/listarLivros";
+		}
+	}
+
+	@GetMapping("/sair")
+	public String sair(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+
+	@GetMapping("/acessoNegado")
+	public String acessoNegado() {
+		return "acesso_negado";
+	}
+}
